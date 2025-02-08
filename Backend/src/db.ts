2715d18e1edb7e6dd6e2dc00543bdf4956524214
Mohfazam <mohd.sarwar.code@gmail.com>
2025-02-09 02:38:24 +0530

    Content Schema

--- Backend/src/db.ts
+++ Backend/src/db.ts
@@ -1,8 +1,17 @@
 import mongoose, { Schema } from "mongoose";
+import { object } from "zod";
 
 const userSchema = new Schema({
     username: {type: String, required: true, unique: true},
     password: {type: String, required: true}
 });
 
+const ContentSchema = new Schema({
+    title: {type: String, required: true},
+    link: {type: String, required: true},
+    tags: [{type: mongoose.Types.ObjectId, ref: "Tag"}],
+    userId: {type: mongoose.Types.ObjectId, ref: "User", required: true}
+})
+
 export const UserModel = mongoose.model("User", userSchema);
+export const ContentModel = mongoose.model("Content", ContentSchema);
