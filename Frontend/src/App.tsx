4467eb527d6553ef9d79cb8be1d58bf7d29b4fe9
Mohfazam <mohd.sarwar.code@gmail.com>
2025-02-24 15:52:45 +0530

    Button basic Functionality

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,6 +1,6 @@
 
 import {Button} from './components/ui/Button'
-import {Plus} from './icons/PlusIcon'
+// import {Plus} from './icons/PlusIcon'
 
 
 
@@ -9,8 +9,7 @@ function App() {
 
   return (
     <div className='2-full h-[900px] flex justify-center items-center gap-6'>
-      <Button startIcon={<Plus size='sm' />} size="sm" variant='primary' text='Add Link'></Button>
-      <Button startIcon={<Plus size='md' />} size="md" variant='secondary' text='Share Link'></Button>
+      <Button varient='primary' text='add content'></Button>
       {/* <Button startIcon={<Plus size='lg' />} size="lg" variant='secondary' text='hyderabad'></Button> */}
     </div>
   )
