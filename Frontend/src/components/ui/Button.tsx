1e4e02b11643db35b1a3d9c3534c23efe5b6da9b
Mohfazam <mohd.sarwar.code@gmail.com>
2025-02-24 15:48:36 +0530

    Colors of Button

--- Frontend/src/components/ui/Button.tsx
+++ Frontend/src/components/ui/Button.tsx
@@ -7,6 +7,13 @@ interface Buttonprops{
     endIcon: ReactElement;
 }
 
-export function Button(props: Buttonprops){
+const vareintClasses = {
+    "primary":"bg-purple-600 text-white",
+    "secondary":"bg-purple-200 text-purple-400"
+};
 
+export function Button({varient, text, startIcon, endIcon}: Buttonprops){
+    return <button className={vareintClasses[varient]}>
+        {text}
+    </button>
 }
