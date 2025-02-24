dd6b0959442aacc1f21de080f457f14fbbe01aad
Mohfazam <mohd.sarwar.code@gmail.com>
2025-02-24 15:30:45 +0530

    Restarted Button.tsx

--- Frontend/src/components/ui/Button.tsx
+++ Frontend/src/components/ui/Button.tsx
@@ -1,32 +1,12 @@
 import { ReactElement } from "react";
 
-type Variants = "primary" | "secondary";
-
-
-interface ButtonProps{
-    variant: Variants;
-    size: "sm" | "md" | "lg";
+interface Buttonprops{
+    varient: "primary" | "secondary";
     text: string;
-    startIcon?: ReactElement;
-    endIcon?: any;
-    onClick?: () => void;
+    startIcon: ReactElement;
+    endIcon: ReactElement;
 }
 
-const variantStyles =  {
-    "primary": "bg-purple-600 text-white",
-    "secondary": "bg-purple-300 text-purple-600",
-}
+export function Button(props: Buttonprops){
 
-const defaultStyles = "rounded-md flex";
-
-const defaultSize = {
-    "sm" : "py-1 px-2",
-    "md": "py-2 px-4",
-    "lg": "py-4 px-6"
-}
-
-export const Button = (props: ButtonProps) => {
-    return <button className={`${variantStyles[props.variant]} ${defaultStyles} ${defaultSize[props.size]}`}>
-        {props.startIcon ? <div className="pr-2">{props.startIcon}</div> : null } {props.text}
-        </button>
 }
