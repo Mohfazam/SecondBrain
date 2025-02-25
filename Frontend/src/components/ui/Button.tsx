64726be5596274d788d363ad48a67dd98409112f
Mohfazam <mohd.sarwar.code@gmail.com>
2025-02-25 10:45:21 +0530

    Button Almost Ready

--- Frontend/src/components/ui/Button.tsx
+++ Frontend/src/components/ui/Button.tsx
@@ -12,10 +12,10 @@ const vareintClasses = {
     "secondary":"bg-purple-200 text-purple-600"
 };
 
-const defaultStyles = "px- py-2 rounded-md"
+const defaultStyles = "px-4 py-2 rounded-md"
 
 export function Button({varient, text, startIcon, endIcon}: Buttonprops){
-    return <button className={`${vareintClasses[varient]}${defaultStyles}`}>
+    return <button className={`${vareintClasses[varient]} ${defaultStyles}`}>
         {text}
     </button>
 }
