92d93dcf9c07cc8aa0145a6ee463e05e12cdabf1
Mohfazam <mohd.sarwar.code@gmail.com>
2025-02-26 03:50:48 +0530

    Fixed the Card Component and Completed its functionality

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,22 +1,26 @@
 
-import {Button} from './components/Button'
+import { Button } from './components/Button'
 import { PlusIcon } from './icons/PlusIcons'
 import { ShareIcon } from './icons/ShareIcon'
 import { Card } from "./components/Card"
-import './index.css'
+
 
 
 
 function App() {
 
 
-  return (
-    <div className='2-full h-[900px] flex justify-center items-center gap-6'>
-      <Button varient='primary' text='Add content' startIcon={<PlusIcon />}></Button>
-      <Button varient='secondary' text='Share Brain' startIcon={<ShareIcon />}></Button>
-      <Card />
+  return <div className='p-4'>
+      <div className='flex justify-end gap-4'>
+        <Button varient='primary' text='Add content' startIcon={<PlusIcon />}></Button>
+        <Button varient='secondary' text='Share Brain' startIcon={<ShareIcon />}></Button>
+      </div>
+      <div className='flex gap=2'>
+          <Card type='twitter' link='https://x.com/mohfazam/status/1889201894897422497' title='First HackAthon Win' />
+          <Card type='youtube' link='https://www.youtube.com/watch?v=PxJNNAezY0A' title='Calling U' />
+        </div>
     </div>
-  )
+  
 }
 
 export default App
