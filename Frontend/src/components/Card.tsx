51fcbb9e0e867a9b9f81f94af115c2e427b934b4
Mohfazam <mohd.sarwar.code@gmail.com>
2025-02-25 14:32:39 +0530

    Embedded X and YT link

--- Frontend/src/components/Card.tsx
+++ Frontend/src/components/Card.tsx
@@ -1,6 +1,12 @@
 import { ShareIcon } from "../icons/ShareIcon"
 
-export function Card(){
+interface cardProps{
+    title: string,
+    link: string,
+    type: "Youtube" | "x"
+}
+
+export function Card({title, link, type}: cardProps){
     return(
         <div className="p-4 bg-white rounded-md border-gray-100 max-w-72 border">
             <div className="flex justify-between ">
@@ -8,10 +14,12 @@ export function Card(){
                     <div className="text-gray-500 pr-2">
                         <ShareIcon />
                     </div>
-                    Project Ideas
+                    {title}
                 </div>
                 <div className="flex items-center">
-                    <div className="pr-2 text-gray-500">
+                    <div className="pr-2 text-gray-500" onClick={() => {
+                        navigator
+                    }}>
                         <ShareIcon />
                     </div>
                     <div className="pr-2 text-gray-500">
@@ -19,6 +27,16 @@ export function Card(){
                     </div>
                 </div>
             </div>
+
+            
+            <div className="pt-4">
+            {/* <iframe className="w-full" src="https://www.youtube.com/embed/mc5uTqhGL0U?si=VCC1GwVMurgC7cSY" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe> */}
+
+            <blockquote className="twitter-tweet">
+                <a href="https://t.co/NPbSdjAqaJ"></a>
+            </blockquote> 
+            </div>
+            
         </div>
     )
 }
