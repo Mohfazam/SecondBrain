9d9ae245381696d4985d5a821ba705c0ad54e7f1
Mohfazam <mohd.sarwar.code@gmail.com>
2025-03-01 12:19:58 +0530

    Added Transition

--- Frontend/src/components/Sidebar.tsx
+++ Frontend/src/components/Sidebar.tsx
@@ -6,11 +6,11 @@ import {Brain} from 'lucide-react'
 export function Sidebar() {
     return(
     <div className="h-screen bg-white border-r w-72 fixed top-0 left-0 pl-6">
-        <div className="flex text-2xl pt-4 py-4 items-center">
-        <div className="pr-2 text-purple-600">
+        <div className="flex text-2xl py-4 items-center pt-8">
+        <div className=" pr-2 text-purple-700">
         <Brain size={32} /></div> Second Brain
         </div>
-        <div className="pt-2">
+        <div className="pt-8 pl-4">
         <SidebarItem text="Twitter" icon={<TwitterIcon />}/>
         <SidebarItem text="Youtube" icon={<YoutubeIcon />}/>
         </div>
