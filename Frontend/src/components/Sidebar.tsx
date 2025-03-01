cf15faf9bf5c05d630cafb40f33e06f117f51ff0
Mohfazam <mohd.sarwar.code@gmail.com>
2025-03-01 12:08:39 +0530

    Logo Added and Fixed

--- Frontend/src/components/Sidebar.tsx
+++ Frontend/src/components/Sidebar.tsx
@@ -7,10 +7,10 @@ export function Sidebar() {
     return(
     <div className="h-screen bg-white border-r w-72 fixed top-0 left-0 pl-6">
         <div className="flex text-2xl pt-4 py-4 items-center">
-        <div className="pr-2">
-        <Brain /></div> Second Brain
+        <div className="pr-2 text-purple-600">
+        <Brain size={32} /></div> Second Brain
         </div>
-        <div className="pt-">
+        <div className="pt-2">
         <SidebarItem text="Twitter" icon={<TwitterIcon />}/>
         <SidebarItem text="Youtube" icon={<YoutubeIcon />}/>
         </div>
