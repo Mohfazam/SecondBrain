e584425b3b8f995588c749bd18d99b739954c8eb
Mohfazam <mohd.sarwar.code@gmail.com>
2025-03-01 12:30:28 +0530

    Fixed Card Component

--- Frontend/src/components/Card.tsx
+++ Frontend/src/components/Card.tsx
@@ -11,9 +11,7 @@ export function Card({title, link, type}: CardProps) {
         <div className="p-4 bg-white rounded-md border-gray-200 max-w-72  border min-h-48 min-w-72">
             <div className="flex justify-between">
                 <div className="flex items-center text-md">
-                    <div className="text-gray-500 pr-2">
-                        <ShareIcon />
-                    </div>
+                    
                     {title}
                 </div>
                 <div className="flex items-center">
@@ -22,9 +20,7 @@ export function Card({title, link, type}: CardProps) {
                             <ShareIcon />
                         </a>
                     </div>
-                    <div className="text-gray-500">
-                        <ShareIcon />
-                    </div>
+                    
                 </div>
             </div>
 
